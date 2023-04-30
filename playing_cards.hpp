84cd66c66440598c2a2c9fad47f50b8e34c5d2a2
SotaNoniwa <99335304+SotaNoniwa@users.noreply.github.com>
2023-04-30 14:01:33 +0200

    Delete playing_cards.hpp

--- playing_cards.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/*Return the number written on the playing card image.
-The function takes gray scale image of playing card. Then calculate its number based on the number of suits (blobs).*/
-int getNumberOnCard(cv::Mat gray_card);
-
-/*Return the suit shown on the playing card image.
-The function takes:*/
-std::string getSuitOnCard(cv::Mat& canvas, std::vector<std::vector<cv::Point>>& contours, std::vector<std::vector<cv::Point>>& conPoly);
